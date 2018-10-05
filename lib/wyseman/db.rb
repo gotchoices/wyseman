6ee95e0b50563b1da839fef953bada9c143ac9c3
Kyle Bateman <kyle@lux2.batemans.org>
2018-10-05 15:05:16 -0600

    Update to work more like the JS version, add parameterized queries

--- lib/wyseman/db.rb
+++ lib/wyseman/db.rb
@@ -4,24 +4,23 @@
 #Copyright WyattERP: GNU GPL Ver 3; see: License in root of this package
 # -----------------------------------------------------------------------------
 #TODO:
-#- Support for more than just english in initialize()
-#- Test if args passed in, like port, dbname
-#- Test if args passed in as a hash
+#- Test doInsert
+#- Remove deprecated quote function
 #- 
 require 'pg'
+Opers		= ['=', '!=', '<', '<=', '>', '>=', '~', 'in']
 
 module Wyseman
 class DB < PG::Connection
   def initialize (opts = {})
     bootstrap = File.join(File.dirname(__FILE__), '..' , 'bootstrap.sql')
-    schema = opts[:schema] || bootstrap
+    schema = opts[:schema] || bootstrap			#Allow user-specified bootstrap file
     opts.delete(:schema)
     devel = opts[:devel]
     opts.delete(:devel)
     
 #puts "Opts:#{opts} schema:#{schema}"
     begin
-      @lang = 'en'					#Fixme: support for multi-language
       super(opts)					#Initialize DB
     rescue PG::ConnectionBad				#If can't connect
       (tmpOpts = opts.dup)[:dbname] = 'template1'
@@ -33,35 +32,40 @@ class DB < PG::Connection
       super(opts)					#And try reconnecting
     end
 
-    set_notice_receiver { |res|
+    set_notice_receiver { |res|				#Callback for DB async notices
       puts res.error_message().split("\n")[0];		#Strip out any CONTEXT: lines
     }
       
     begin
       release = one("select wm.release();")[0]		#If wyseman schema doesn't exist
-    rescue PG::InvalidSchemaName			#create it
+    rescue PG::InvalidSchemaName			#create it from an app-specific schema setup
 #puts "Schema file:#{schema}"
       t(File.open(schema,'r').read)
     end
     
     begin
       one("select max(release) from wm.releases;")	#If development schema doesn't exist
-    rescue PG::UndefinedTable				#create it
+    rescue PG::UndefinedTable				#create it, if development switch specified
       t(File.open(bootstrap,'r').read)
     end if devel
     
-    @column_data = {}
-    @table_data = {}
+    @column_data = {}					#cache of column information
+    @table_data = {}					#and table information
   end
 
 # -----------------------------------------------------------------------------
-  def x(query)						#Short-hand for exec
-      exec(query)
+  def x(query, parms = nil)				#Short-hand for exec
+      exec_params(query, parms)
   end
 
 # -----------------------------------------------------------------------------
   def t(query)						#Exec query as atomic transaction
       transaction { |c| c.exec(query)}
+  end
+
+# -----------------------------------------------------------------------------
+  def one(query, parms = nil)				#Get a single row as an array
+      exec_params(query, parms).values[0]
   end
 
 # -----------------------------------------------------------------------------
@@ -74,30 +78,26 @@ class DB < PG::Connection
       quote_ident (str)
   end
 
+# Deprecated
 # -----------------------------------------------------------------------------
-  def one(query)					#Get a single row as an array
-      exec(query).values[0]
-  end
-
-# -----------------------------------------------------------------------------
-  def quote(tab, col, val, errchk = false)		#Return a value with single quote, if 
-    return val if col == 'oid'
-#printf("tab:%s col:%s val:%s\n", tab, col, val)
-    tp = (cdat = column(tab, col))['type']
-#printf("  cdat:%s\n", cdat)
-    return 'null' if val == '' && cdat['nonull'] != 't'
-    if %w{numeric int int4 int8 float float4 float8}.include?(tp)
-      val = val.gsub(/[$,]/,'')
-      if val == ''
-        raise "Illegal blank value for table:#{tab} column:#{col}" if errchk
-        return 'null'
-      end
-      return val
-    elsif tp[0] == '_'
-      return "'#{esc(val)}'"
-    end
-    return "'" + (escape_string (val)) + "'"
-  end
+#  def quote(tab, col, val, errchk = false)		#Return a value with single quote, if 
+#    return val if col == 'oid'
+##printf("tab:%s col:%s val:%s\n", tab, col, val)
+#    tp = (cdat = column(tab, col))['type']
+##printf("  cdat:%s\n", cdat)
+#    return 'null' if val == '' && cdat['nonull'] != 't'
+#    if %w{numeric int int4 int8 float float4 float8}.include?(tp)
+#      val = val.gsub(/[$,]/,'')
+#      if val == ''
+#        raise "Illegal blank value for table:#{tab} column:#{col}" if errchk
+#        return 'null'
+#      end
+#      return val
+#    elsif tp[0] == '_'
+#      return "'#{esc(val)}'"
+#    end
+#    return "'" + (escape_string (val)) + "'"
+#  end
   
 # -----------------------------------------------------------------------------
   def table_split(tab)				#Split schema, table into array
@@ -107,21 +107,21 @@ class DB < PG::Connection
 
 # -----------------------------------------------------------------------------
   def style(tab, col=nil)		# Return table or column default styles
-    #Port_me
+    #Port_me?
   end
 
 # -----------------------------------------------------------------------------
   def view_oid (tab)			# Return name of an oid column (typically _oid) for a view
-    #Port_me
+    #Port_me?
   end
 
 # -----------------------------------------------------------------------------
   def message_text (tab, code)		# Return the text for a specified message
-    #Port_me
+    #Port_me?
   end
 
 # -----------------------------------------------------------------------------
-  def table(tab)			# Return table text and type
+  def table(tab)			# Return meta-data about tables
     idx = tab
 #printf("table tab:%s\n", tab)
     if !@table_data[idx]
@@ -137,13 +137,13 @@ class DB < PG::Connection
   end
 
 # -----------------------------------------------------------------------------
-  def column(tab, col = nil)	# Return hash containing column text and type, or all columns
+  def column(tab, col = nil)		# Return hash containing column meta-data, or all columns
     if col
       idx = tab + ':' + col
 #printf("column tab:%s col:%s idx:%s\n", tab, col, idx)
       if !@column_data[idx] then
         s, t = table_split(tab)
-        self.x("select col,title,help,type,nonull from wm.column_pub where sch = '#{esc(s)}' and tab = '#{esc(t)}' and language = '#{@lang}';").each { |rec|
+        self.x("select cdt_col as col,field,type,nonull,def,length,pkey from wm.column_data where cdt_sch = '#{esc(s)}' and cdt_tab = '#{esc(t)}';").each { |rec|
           ix = tab + ':' + rec['col']
           rec.delete('col')
           @column_data[ix] = rec
@@ -159,90 +159,109 @@ class DB < PG::Connection
 
 # -----------------------------------------------------------------------------
   def column_values(tab, col, value=nil)	# Return allowable values for a column if they exist
-    #Port_me
+    #Port_me?
   end
 
 # -----------------------------------------------------------------------------
   def tables_ref(tab, refme=false)	# Return tables that are referenced (pointed to) by the specified table
-    #Port_me				# If refme true, return tables that reference the specified table
+    #Port_me?				# If refme true, return tables that reference the specified table
   end
 
 # -----------------------------------------------------------------------------
   def columns_fk(tab, ftab)		# Return the fk columns in a table and the pk columns they point to in a foreign table
-    #Port_me
+    #Port_me?
   end
 
 # -----------------------------------------------------------------------------
-  def comp_list(fdata, tab, func='=')	#Generate a list of fields = val
-    conds = []
-    fdata.each_pair { |idx, val|
-      conds << self.qid(idx) + " #{func} " + self.quote(tab,idx,val)
+  def comp_list(fdata, res, func='=')	#Generate a list of fields = val
+    i = res[:parms].length + 1
+    conds = []; fdata.each_pair { |idx, val|
+      conds << quote_ident(idx.to_s) + " #{func} $" + i.to_s; i += 1
+      res[:parms] << val
     }
     conds
   end
 
 # -----------------------------------------------------------------------------
-  def doSelect(fields, tab=nil, where=nil)	#Run a select from given parameters
-#printf("Select fields:%s tab:%s where:%s\n", fields, tab, where)
-    frtab = tab ? " from " + tab : ''
-    if where.is_a?(Hash)
-      where = comp_list(where,tab).join(' and ')
-    elsif where.is_a?(Array)
-      where = where.join(' and ')
-    elsif !where
-      where = ''
+  def buildWhere(logic, res)				#Generate a where clause and its parameters
+    i = res[:parms].length + 1				#parameter counter
+    if logic.is_a?(String)				#Explicit where clause (potential security hole)
+      return(logic)
+    elsif logic.key?('items')
+      logic[:and] = true if !logic.key?('and')		#Default to ands
+      clauses = []
+      logic[:items].each { |item| clauses << buildWhere(item, res)}
+      return clauses.join(logic[:and] ? ' and ' : ' or ')
+    elsif logic.key?('left')
+      oper = logic[:oper] || '='
+      raise "Invalid operator: #{oper}" if !Opers.include?(oper)
+      res[:parms] << (logic[:entry] || logic[:right] || '')
+      clause = "#{quote_ident(logic.left)} #{oper} $#{i}"; i += 1
+      return clause
+    elsif logic.is_a?(Hash)
+      clauses = []
+      logic.each_pair { |key, val|
+        clauses << "#{quote_ident(key.to_s)} = $#{i}"; i += 1
+        res[:parms] << val
+      }
+      return clauses.join(' and ')
     end
-    where = " where " + where if where != ''
-    query = 'select ' + fields + frtab + where + ';'
-#printf("Select query:%s\n", query)
-    self.x query
+    raise "mangled logic: #{logic}"
+  end
+
+# -----------------------------------------------------------------------------
+  def doSelect(fields, tab=nil, logic=nil)	#Run a select from given parameters
+#printf("Select fields:%s tab:%s logic:%s\n", fields, tab, logic)
+    res = {parms:[]}
+    frtab = tab ? " from " + tab.split('.').map{|n| quote_ident(n)}.join('.') : ''
+    where = buildWhere(logic, res)
+    wwhere = " where " + where if where != ''
+    query = 'select ' + fields + frtab + wwhere + ';'
+#printf("Select query:%s parms:%s\n", query, res[:parms])
+    exec_params(query, res[:parms])
   end
 
 # -----------------------------------------------------------------------------
   def doInsert(tab, data)		#Insert a record contained in a hash
-#printf("Insert tab:%s data:%s\n", tab, data)
-    fields, values = [], []
-    data.each_pair { |idx, val|
-        fields << self.qid(idx)
-        values << self.quote(tab,idx,val)
+printf("Insert tab:%s data:%s\n", tab, data)
+    parms = []
+    i = parms.length + 1				#parameter counter
+    fields = [], values = []
+    data.each_pair { |key, val|
+        fields << self.qid(key)
+        values << "$" + i.to_s; i += 1
+        parms << val
     }
-    sql = "insert into #{tab} (#{fields.join(',')}) values (#{values.join(',')}) returning *;"
-puts 'Test_me:' + sql
-   res = self.x(sql)
-   raise 'Error inserting #{sql}' if res.ntuples != 1
-   res[0]
+    sql = "insert into #{tab.split('.').map{|n| quote_ident(n)}.join('.')} (#{fields.join(',')}) values (#{values.join(',')}) returning *;"
+printf "Test_me:%s :%s\n", sql, res[:parms]
+#   res = exec_params(sql, parms)
+#   raise 'Error inserting #{sql}' if res.ntuples != 1
+#   res[0]
   end
 
 # -----------------------------------------------------------------------------
-  def doUpdate(tab, data, where)	#Update records as specified in a hash
-#printf("Update table:%s data:%s where:%s\n", tab, data, where)
-    setems = comp_list(data, tab)
+  def doUpdate(tab, data, logic)	#Update records as specified in a hash
+#printf("Update table:%s data:%s logic:%s\n", tab, data, logic)
+    res = {parms:[]}
+    setems = comp_list(data, res)
     return nil if setems.length <= 0
-    if where.is_a?(Hash)
-      where = comp_list(where,tab).join(' and ')
-    elsif where.is_a?(Array)
-      where = where.join(' and ')
-    end
+    where = buildWhere(logic, res)
     raise 'Illegal where clause' if !where
-    where = "where " + where if where != ''
-    sql = "update #{tab} set #{setems.join(',')} #{where};"
-#puts 'Test_me:' + sql
-    self.x sql
+    wwhere = "where " + where if where != ''
+    sql = "update #{tab.split('.').map{|n| quote_ident(n)}.join('.')} set #{setems.join(',')} #{wwhere};"
+printf "Test_me:%s :%s\n", sql, res[:parms]
+    exec_params(sql, res[:parms])
   end
 
 # -----------------------------------------------------------------------------
   def doDelete(tab, where)	#Delete records from a table
-printf("Delete from table:%s where:%s\n", tab, where)
-    if where.is_a?(Hash)
-      conds = comp_list(where,tab).join(' and ')
-    elsif where.is_a?(Array)
-      where = where.join(' and ')
-    end
-    raise 'Illegal where clause' if !where or where == ''
-    where = "where " + where
-    sql = "delete from #{tab} #{where};"
-puts 'Test_me:' + sql
-#    self.x sql
+#printf("Delete from table:%s where:%s\n", tab, where)
+    res = {parms:[]}
+    wh = buildWhere(where, res)
+    raise 'unbounded delete' if res[:parms].length <= 0
+    sql = "delete from #{tab.split('.').map{|n| quote_ident(n)}.join('.')} where #{wh};"
+printf "Test_me:%s :%s\n", sql, res[:parms]
+    exec_params(sql, res[:parms])
   end
 
 end	#class DB
